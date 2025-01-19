cb88919f06fa09af92834613c817d2b4d0f842b3
william <william.mine64@gmail.com>
2025-01-18 22:23:30 -0300

    update

--- .config/nvim/lua/night/remap.lua
+++ .config/nvim/lua/night/remap.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Bind <leader>f to vim.lsp.buf.format()
-vim.api.nvim_set_keymap('n', '<Leader>f', '<Cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('n', 'F', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' },
 	  keys = {
-		{"<leader>ff", "<cmd>Telescope find_files<cr>"},
-		{"<leader>fg", "<cmd>Telescope live_grep<cr>"}		
+		{"<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Fuzzy finder"},
+		{"<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Fuzzy find with grep"}		
 	  },
 }
